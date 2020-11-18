43b56f9eaa81798a261443c0edec6611ee9032be
samuel14find <bicalho.sam@gmail.com>
2020-11-18 12:45:58 -0300

    Controller Inicial.

---
+++ BootCamp/BootCamp/BootCamp/Controllers/AlbumController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAlguns()
+        {
+            return Ok(new
+            {
+                Message = "Primeira Api Criada"
+            });
+        }
+    }
+}
