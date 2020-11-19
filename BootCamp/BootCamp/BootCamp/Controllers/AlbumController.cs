20a7f94b696230dfba740ec7f4ec94be4400ab33
samuel14find <bicalho.sam@gmail.com>
2020-11-19 20:37:24 -0300

    Começando a utilizar o Repository. Faço una injeção de dependência. E minha action retorna Ok com todos os albuns. Ainda não fazemos tratamento de erro.

--- BootCamp/BootCamp/BootCamp/Controllers/AlbumController.cs
+++ BootCamp/BootCamp/BootCamp/Controllers/AlbumController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BootCamp.Repository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,13 +12,15 @@ namespace BootCamp.Controllers
     [ApiController]
     public class AlbunsController : ControllerBase
     {
+        private AlbumRepository _ctx { get; init; }
+        public AlbunsController(AlbumRepository ctx)
+        {
+            this._ctx = ctx;
+        }
         [HttpGet]
         public async Task<IActionResult> GetAlbuns()
         {
-            return Ok(new
-            {
-                Message = "Primeira Api Criada"
-            });
+            return Ok((await this._ctx.GetAllAsync()));
         }
     }
 }
