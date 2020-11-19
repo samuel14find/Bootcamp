9dd48acd6b63376b566df67cd903a0add406b763
samuel14find <bicalho.sam@gmail.com>
2020-11-19 19:55:13 -0300

    Essas classes representam nosso domínio. Criamos a entidade Algum e Music. Observar as propriedades de navegação public Album Album e public IList<Music> Musics. Ou seja uma musica vai estar ligado à um album. E um algum terá uma lista de muscs.

---
+++ BootCamp/BootCamp/BootCamp/Model/Album.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootCamp.Model
+{
+    public class Album
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public String Description { get; set; }
+        public String Backdrop { get; set; }
+        public String Band { get; set; }
+        public IList<Music> Musics { get; set; }
+
+    }
+}
---
+++ BootCamp/BootCamp/BootCamp/Model/Music.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootCamp.Model
+{
+    public class Music
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public int Duration { get; set; }
+        public Album Album { get; set; }
+    }
+}
