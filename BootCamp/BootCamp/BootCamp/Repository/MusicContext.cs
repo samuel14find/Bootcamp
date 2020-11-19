05e0edbf5a07274e17dd32e0cf4fff34a27a8d2b
samuel14find <bicalho.sam@gmail.com>
2020-11-19 20:10:18 -0300

    Aqui temos o contexto da minha aplicação, herdando de DbContext. Observar que aqui que configuro o Mapping, através do método OnModelCreating().

---
+++ BootCamp/BootCamp/BootCamp/Repository/MusicContext.cs
@@ -0,0 +1,27 @@
+﻿using BootCamp.Model;
+using BootCamp.Repository.Mapping;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootCamp.Repository
+{
+    public class MusicContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public MusicContext(DbContextOptions<MusicContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AlbumMapping());
+            modelBuilder.ApplyConfiguration(new MusicMapping());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
