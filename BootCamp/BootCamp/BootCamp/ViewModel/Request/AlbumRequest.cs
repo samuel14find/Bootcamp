a989497648519f2828947cb2bc1d67b1241751e8
samuel14find <bicalho.sam@gmail.com>
2020-11-20 13:18:57 -0300

    Criamos nossa View Model para não poluir a Model do domínio. Aqui faço validações.

---
+++ BootCamp/BootCamp/BootCamp/ViewModel/Request/AlbumRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootCamp.ViewModel.Request
+{
+    public class AlbumRequest
+    {
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        public String Band { get; set; }
+        [Required]
+        public String Description { get; set; }
+        [Required]
+        public String Backdrop { get; set; }
+        [Required]
+        public List<MusicRequest> Musics { get; set; }
+
+    }
+}
