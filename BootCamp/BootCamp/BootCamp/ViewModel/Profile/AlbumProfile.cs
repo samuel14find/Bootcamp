48b6dd4fe78bb6720b5bd69be0ffefff53fa176e
samuel14find <bicalho.sam@gmail.com>
2020-11-20 15:41:35 -0300

    Classe utilizada pelo AutoMapper para mapeamento dos profiles

---
+++ BootCamp/BootCamp/BootCamp/ViewModel/Profile/AlbumProfile.cs
@@ -0,0 +1,18 @@
+﻿using BootCamp.Model;
+using BootCamp.ViewModel.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootCamp.ViewModel.Profile
+{
+    public class AlbumProfile: AutoMapper.Profile
+    {
+        public AlbumProfile()
+        {
+            CreateMap<MusicRequest, Music>();
+            CreateMap<AlbumRequest, Album>();
+        }
+    }
+}
