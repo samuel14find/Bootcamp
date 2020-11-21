641e95444875325b12bcbb4eb7675576deffa06b
samuel14find <bicalho.sam@gmail.com>
2020-11-21 14:33:58 -0300

    Adicionamos as configurações para o Entity Frameworl trabalhar o User e Favorit Music.

---
+++ BootCamp/BootCamp/BootCamp/Repository/Mapping/UserFavoriteMusicMapping.cs
@@ -0,0 +1,26 @@
+﻿using BootCamp.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootCamp.Repository.Mapping
+{
+    public class UserFavoriteMusicMapping : IEntityTypeConfiguration<UserFavoritMusic>
+    {
+        public void Configure(EntityTypeBuilder<UserFavoritMusic> builder)
+        {
+            builder.ToTable("UserFavoritMusic");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.MusicId);
+
+            // Aqui vou fazer o mapeamento para as músicas. Só vou ter uma música favorita
+            builder.HasOne(x => x.Music)
+                .WithOne()
+                .HasForeignKey<UserFavoritMusic>(x => x.MusicId);
+        }
+    }
+}
---
+++ BootCamp/BootCamp/BootCamp/Repository/Mapping/UserMapping.cs
@@ -0,0 +1,30 @@
+﻿using BootCamp.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootCamp.Repository.Mapping
+{
+    public class UserMapping : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User"); // Builder vai trabalhar com uma tabela chamada User
+            builder.HasKey(x => x.Id); // Ela tem uma chave primária
+            builder.Property(x => x.Id).ValueGeneratedOnAdd(); // O valor da chave vai ser adicionado na hora da inserção
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(200); // Email obrigatório e seu tamanho máximo é 200
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(200); //..
+            builder.Property(x => x.Password).IsRequired().HasMaxLength(200); //..
+            builder.Property(x => x.Photo).IsRequired().HasMaxLength(500); //.. 
+            // Se eu deletar um usuário não faz sentido ter música favorita dele
+            builder.HasMany(x => x.FavoritMusic)
+                .WithOne(x => x.User)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+        }
+    }
+}
