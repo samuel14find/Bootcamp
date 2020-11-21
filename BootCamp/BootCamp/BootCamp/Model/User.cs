f70367645705c78345e571e011677901a61f5e92
samuel14find <bicalho.sam@gmail.com>
2020-11-21 09:54:16 -0300

    Adicionamos algumas regras de negócio. Primeiro adicionamos uma IList que irá representar as músicas favoritadas do usuário. Depois criamos regra de negócio para addicionar essa música favorita e depois uma regra para remover essa música favorita

--- BootCamp/BootCamp/BootCamp/Model/User.cs
+++ BootCamp/BootCamp/BootCamp/Model/User.cs
@@ -13,5 +13,27 @@ namespace BootCamp.Model
         public String Password { get; set; }
         public String Photo { get; set; }
 
+        public IList<UserFavoritMusic> FavoritMusic {get; set;}
+
+        public void AddFavoritMusic(Music music)
+        {
+            this.FavoritMusic.Add(new UserFavoritMusic()
+            {
+                Music = music,
+                MusicId = music.Id,
+                User = this,
+                UserId = this.Id
+            });
+        }
+
+        public void RemoveFavoriteMusic(Music music)
+        {
+            var favMusic = this.FavoritMusic
+                               .Where(x => x.MusicId == music.Id)
+                               .FirstOrDefault();
+            if (favMusic == null)
+                throw new Exception("Música não localizada na lista de favoritos");
+            this.FavoritMusic.Remove(favMusic);        }
+
     }
 }
