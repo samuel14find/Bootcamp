14907713cfeb84dfc2bf83bf163fa5bc30b9a765
samuel14find <bicalho.sam@gmail.com>
2020-11-21 09:11:27 -0300

    Entidade representando o usuário do sistema

---
+++ BootCamp/BootCamp/BootCamp/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootCamp.Model
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String Photo { get; set; }
+
+    }
+}
