8fb7396baf8e95e5c695fb331f911b63fba620a2
samuel14find <bicalho.sam@gmail.com>
2020-11-22 13:58:49 -0300

    Criamos uma Viewmodel para quando o usuário entrar no sistema. Preenchendo o email e a senha.

---
+++ BootCamp/BootCamp/BootCamp/ViewModel/Request/SignInRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootCamp.ViewModel.Request
+{
+    public class SignInRequest
+    {
+        /// Como vamos lidar com o usuário entrando no sistema basicamente vamos ter 
+        /// os campos email e password
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+        [Required]
+        public String Password { get; set; }
+
+
+    }
+}
