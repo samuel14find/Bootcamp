6cb5aa42f9cc510e1f2b2fe9e262fdeffc8ffae2
samuel14find <bicalho.sam@gmail.com>
2020-11-22 13:56:08 -0300

    Essa será uma classe View Model responsável por carregar a música favorita do usuário. Notar que ela tem propriedade tanto da Music quanto de Album. Ai lá no UserProfile vou ter que fazer o mapeamento de UserFavoritMusic para FavoritMusicResponse

---
+++ BootCamp/BootCamp/BootCamp/ViewModel/Response/FavoritMusicResponse.cs
@@ -0,0 +1,23 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootCamp.ViewModel.Response
+{
+    public class FavoritMusicResponse
+    {
+        /// Carregando as músicas do User. 
+        
+        public Guid Id { get; set; }
+        public Guid MusicId { get; set; }
+        public String Name { get; set; }
+        public int Duration { get; set; }
+
+        public String AlbumName { get; set; }
+        public String Band { get; set; }
+        public String Backdrop { get; set; }
+        public String AlbumId { get; set; }
+
+    }
+}
