3f9bc968bd4299f4333ab88d0258cb5c350d1d13
samuel14find <bicalho.sam@gmail.com>
2020-11-22 10:07:55 -0300

    Criamos essas ViewModel para responder ao usuário quando ele fizer o login. Ou seja se o usuário se autenticar com sucesso, vou ter as informações de Id dele, Name, Email e Photo.

---
+++ BootCamp/BootCamp/BootCamp/ViewModel/Response/UserResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootCamp.ViewModel.Response
+{
+    public class UserResponse
+    {
+        public Guid Id { get; set; }
+        public String  Name { get; set; }
+        public String Email { get; set; }
+        public String Photo { get; set; }
+    }
+}
