9f75cc84830f5cd96266cbf62a5f53e1c022aad8
samuel14find <bicalho.sam@gmail.com>
2020-11-22 13:57:30 -0300

    Criamos o Mapeamento da UserFavoritMusic para FavoritMusicResponse, percorrendo as propriedades com o método ForMember().

--- BootCamp/BootCamp/BootCamp/ViewModel/Profile/UserProfile.cs
+++ BootCamp/BootCamp/BootCamp/ViewModel/Profile/UserProfile.cs
@@ -1,4 +1,4 @@
-﻿using BootCamp.Model;
+﻿ using BootCamp.Model;
 using BootCamp.ViewModel.Request;
  using BootCamp.ViewModel.Response;
 using System;
@@ -12,6 +12,17 @@ namespace BootCamp.ViewModel.Profile
     {
         public UserProfile()
         {
+            ///          Quem é a fonte / Mapea para 
+            CreateMap<UserFavoritMusic, FavoritMusicResponse>()
+                .ForMember(x => x.AlbumId, m => m.MapFrom(f => f.Music.Album.Id))
+                .ForMember(x => x.AlbumName, m => m.MapFrom(f => f.Music.Album.Name))
+                .ForMember(x => x.Backdrop, m => m.MapFrom(f => f.Music.Album.Backdrop))
+                .ForMember(x => x.Band, m => m.MapFrom(f => f.Music.Album.Band))
+                .ForMember(x => x.Duration, m => m.MapFrom(f => f.Music.Duration))
+                .ForMember(x => x.Id, m => m.MapFrom(f => f.Id))
+                .ForMember(x => x.MusicId, m => m.MapFrom(f => f.MusicId))
+                .ForMember(x => x.Name, m => m.MapFrom(f => f.Music.Name));
+
             CreateMap<RegisterRequest, User>();
             CreateMap<User, UserResponse>();
         }
