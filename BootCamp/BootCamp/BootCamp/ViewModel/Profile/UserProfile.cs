2c5f9dbea46d254014a04bde4d399c68bb96b6ad
samuel14find <bicalho.sam@gmail.com>
2020-11-22 10:10:09 -0300

    Criamos essse Maper para trabalhar o Mapeamente entre RegisterRequest(view model) e o User(classe da model)

---
+++ BootCamp/BootCamp/BootCamp/ViewModel/Profile/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using BootCamp.Model;
+using BootCamp.ViewModel.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootCamp.ViewModel.Profile
+{
+    public class UserProfile: AutoMapper.Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<RegisterRequest, User>();
+        }
+        
+    }
+}
