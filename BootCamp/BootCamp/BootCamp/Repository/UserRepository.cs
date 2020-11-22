c739c1db9b4172748dbb69c4b206fd68ec9f3a1a
samuel14find <bicalho.sam@gmail.com>
2020-11-22 10:38:37 -0300

    Criamos o método CreateAsync para salvar um novo usuário na base de dados.

---
+++ BootCamp/BootCamp/BootCamp/Repository/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using BootCamp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootCamp.Repository
+{
+    public class UserRepository
+    {
+        private MusicContext _ctx { get; init; }
+
+        public UserRepository(MusicContext ctx)
+        {
+            this._ctx = ctx;
+        }
+
+        public async Task CreateAsync(User user)
+        {
+            await this._ctx.Users.AddAsync(user);
+            await this._ctx.SaveChangesAsync(); 
+        }
+    }
+}
