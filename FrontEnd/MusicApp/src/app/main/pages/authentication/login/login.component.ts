9d9420c1d5919a9f1a88f6ed59f95da7b0ca3775
samuel14find <bicalho.sam@gmail.com>
2020-11-23 13:41:50 -0300

    Chamos o método login() para logar o usuário

--- FrontEnd/MusicApp/src/app/main/pages/authentication/login/login.component.ts
+++ FrontEnd/MusicApp/src/app/main/pages/authentication/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 
 import { FuseConfigService } from "@fuse/services/config.service";
+import SignIn from 'app/model/signIn';
+import { UserService } from 'app/services/user.service';
 import swal from "sweetalert2";
 
 @Component({
@@ -15,7 +17,8 @@ export class LoginComponent implements OnInit {
     loginForm: FormGroup;
     constructor(
         private fuseConfigService: FuseConfigService,
-        private formBuilder: FormBuilder
+        private formBuilder: FormBuilder,
+        private userService: UserService
     ) {
         this.fuseConfigService.config = {
             layout: {
@@ -40,5 +43,17 @@ export class LoginComponent implements OnInit {
             email: ["", [Validators.required, Validators.email]],
             password: ["", Validators.required],
         });
+    }
+
+    login(loginForm){
+        if(loginForm.isValid == false) return;
+
+        let email = this.loginForm.get("email").value;
+        let password = this.loginForm.get("password").value; 
+        let signIn = new SignIn();
+        signIn.email = email;
+        signIn.password=password;
+
+        this.userService.authenticate(signIn).subscribe(result=>{})
     }
 }
