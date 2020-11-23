8d5197612b2d9203a7b4993d9e4a8900dae95a2d
samuel14find <bicalho.sam@gmail.com>
2020-11-23 12:00:27 -0300

    Adicionamos os modulos da trabalhar com a interface dos detalhes da musica

--- FrontEnd/MusicApp/src/app/main/pages/music/music.module.ts
+++ FrontEnd/MusicApp/src/app/main/pages/music/music.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { MusicRoutingModule } from './music-routing.module';
 import { MusicComponent } from './music.component';
 import { FuseSharedModule } from '@fuse/shared.module';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatListModule } from '@angular/material';
 import { MusicDetailComponent } from './music-detail/music-detail.component';
 
 @NgModule({
@@ -16,7 +16,9 @@ import { MusicDetailComponent } from './music-detail/music-detail.component';
     CommonModule,
     MusicRoutingModule,
     FuseSharedModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
   ]
 })
 export class MusicModule { }
