1c81c4416b1336f8b4b27c2884789624331b66f8
samuel14find <bicalho.sam@gmail.com>
2020-11-23 20:25:41 -0300

    Adicionamos um método para remover a música dos favoritos do usuário

--- FrontEnd/MusicApp/src/app/main/pages/favorite/favorite.component.ts
+++ FrontEnd/MusicApp/src/app/main/pages/favorite/favorite.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import User from 'app/model/user';
+import { PersistedStateService } from 'app/services/persisted-state.service';
+import { UserService } from 'app/services/user.service';
 import Swal from "sweetalert2";
 
 @Component({
@@ -8,10 +11,10 @@ import Swal from "sweetalert2";
     styleUrls: ["./favorite.component.scss"],
 })
 export class FavoriteComponent implements OnInit {
+    user: User;
     displayedColumns: string[] = [
         "position",
         "name",
-        "duration",
         "band",
         "avatar",
         "album",
@@ -19,7 +22,34 @@ export class FavoriteComponent implements OnInit {
     ];
     dataSource: any[];
 
-    constructor() {}
+    constructor(private persistedState: PersistedStateService, 
+               private router: Router, private userService: UserService) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.user = this.persistedState.get(this.persistedState.LOGGED_IN);
+        this.dataSource = this.user.favoritMusics as any;
+    }
+    goToDetail(albumId){
+        this.router.navigate(["music", albumId]);
+    }
+
+    removeFromFavorite(musicId){
+
+        this.userService.removeFromFavorite(this.user.id, musicId).subscribe(()=>{
+            // Primeiro eu removo a música que acabou se removida do banco de dados. Porque eu preciso 
+            // retirar ela do grid. Sendo assim eu faço um filtro onde o Id é diferente do id que tá favoritado
+            this.user.favoritMusics = this.user.favoritMusics.filter(x=>x.musicId != musicId);
+            // Aqui atualizamos a persistência de dados. A partir do momento que já removi, eu tenho que 
+            // atualizar para manter a consistência de informações. 
+            this.persistedState.set(this.persistedState.LOGGED_IN, this.user);
+            //Aqui eu atualizo a visualização do usuário. Para que quando ele olhar a tabela e ter a sensação 
+            //que a linha foi removida. 
+            this.dataSource = this.user.favoritMusics as any;
+            Swal.fire(
+                "Sucesso!",
+                "Musica Removida dos favoritos",
+                "success"
+            )
+        });  
+    }
 }
