a1dc5aaf3cca1b3bd5f3e00f4efa9efd04eaab43
samuel14find <bicalho.sam@gmail.com>
2020-11-22 23:11:58 -0300

    Adicionamos um novo routing para atender os detalhes da music

--- FrontEnd/MusicApp/src/app/main/pages/music/music-routing.module.ts
+++ FrontEnd/MusicApp/src/app/main/pages/music/music-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MusicDetailComponent } from './music-detail/music-detail.component';
 import { MusicComponent } from './music.component';
 
-const routes: Routes = [{
+const routes: Routes = [
+  {
   path: "music",
   component: MusicComponent
-}];
+  },
+  {
+    path: "music/:id",
+    component: MusicDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
