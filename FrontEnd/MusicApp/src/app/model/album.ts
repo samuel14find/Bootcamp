a63b252f7253bf6eebf03c9e13376b3e2ccdb6e0
samuel14find <bicalho.sam@gmail.com>
2020-11-23 11:58:56 -0300

    Adicionamos a propriedade musics para trazer o modelo Music para o Modelo Album

--- FrontEnd/MusicApp/src/app/model/album.ts
+++ FrontEnd/MusicApp/src/app/model/album.ts
@@ -1,7 +1,11 @@
+import Music from './music';
+
 export default class Album {
     public id?: String;
     public name?: String;
     public description?: String;
     public backdrop?: String;
     public band?: String;
+    public musics?: Music[];
+    
 }
