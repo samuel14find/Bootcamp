9f91d9d3db5c125f13af32fab7f99d9f23620c2c
samuel14find <bicalho.sam@gmail.com>
2020-11-23 13:40:48 -0300

    Criamos o modelo para representar as músicas favoritas do usuário

---
+++ FrontEnd/MusicApp/src/app/model/favoriteMusic.ts
@@ -0,0 +1,10 @@
+export default class FavoriteMusic{
+    public id?: String;
+    public musicId?: String;
+    public name?: String;
+    public duration?: Number;
+    public albumId?: String;
+    public albumName?: String;
+    public band?: String;
+    public backdrop?: String;
+}
