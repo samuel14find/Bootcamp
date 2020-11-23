b04a002d11696d2ed199ff5dca20e573e867ff78
samuel14find <bicalho.sam@gmail.com>
2020-11-23 14:18:48 -0300

    Criação de uma variável para ser usada como chave

--- FrontEnd/MusicApp/src/app/services/persisted-state.service.ts
+++ FrontEnd/MusicApp/src/app/services/persisted-state.service.ts
@@ -7,6 +7,8 @@ export class PersistedStateService {
 
   constructor() { }
 
+  public LOGGED_IN: string = "AUTHENTICATED_USER";
+
   public set (key: string, data: any){
     localStorage.setItem(key, JSON.stringify(data));
   }
@@ -19,6 +21,8 @@ export class PersistedStateService {
     localStorage.removeItem(key);
   }
 
+  // !! dentro do JS significa se quero ver a variável, se o objeto é nulo. Ele também verifica se o 
+  // tipo do objeto está nulo.
   public exists(key: string){
     return !!localStorage.getItem(key);
   }
