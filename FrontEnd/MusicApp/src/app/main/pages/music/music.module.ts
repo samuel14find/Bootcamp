4c0952795b8dce7569810a2fed02eb9403317aa7
samuel14find <bicalho.sam@gmail.com>
2020-11-22 23:10:47 -0300

    Importamos o componente MusicDetail

--- FrontEnd/MusicApp/src/app/main/pages/music/music.module.ts
+++ FrontEnd/MusicApp/src/app/main/pages/music/music.module.ts
@@ -5,10 +5,12 @@ import { MusicRoutingModule } from './music-routing.module';
 import { MusicComponent } from './music.component';
 import { FuseSharedModule } from '@fuse/shared.module';
 import { MatButtonModule } from '@angular/material';
+import { MusicDetailComponent } from './music-detail/music-detail.component';
 
 @NgModule({
   declarations: [
-    MusicComponent
+    MusicComponent,
+    MusicDetailComponent
   ],
   imports: [
     CommonModule,
