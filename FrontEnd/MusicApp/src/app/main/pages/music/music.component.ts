36f53c38da5e8cdde7142cbded881bdb0ca34d56
samuel14find <bicalho.sam@gmail.com>
2020-11-22 23:11:30 -0300

    Adicionamos a function detail que vai ser chamda quando o usuário clicar em details.

--- FrontEnd/MusicApp/src/app/main/pages/music/music.component.ts
+++ FrontEnd/MusicApp/src/app/main/pages/music/music.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  Router } from '@angular/router';
 import Album from 'app/model/album';
 import { MusicService } from 'app/services/music.service';
 
@@ -10,13 +11,17 @@ import { MusicService } from 'app/services/music.service';
 export class MusicComponent implements OnInit {
   albuns: Album[] = []
 
-  constructor(private musicService: MusicService) { }
+  constructor(private musicService: MusicService, private router: Router) { }
 
   ngOnInit() {
     this.musicService.getAlbuns().subscribe(result=>{
       this.albuns = result;
       console.log(this.albuns);
     })
+  }
+
+  detail(album){
+    this.router.navigate(["music", album.id]);
   }
 
 }
