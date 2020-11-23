22c11da6c60bc82b541657532c0f85ea29264125
samuel14find <bicalho.sam@gmail.com>
2020-11-22 22:15:05 -0300

    Adicionamos o getAlbuns para recuperar os dados da API usando Observable

--- FrontEnd/MusicApp/src/app/services/music.service.ts
+++ FrontEnd/MusicApp/src/app/services/music.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import Album from 'app/model/album';
+import {environment} from 'environments/environment'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MusicService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAlbuns(): Observable<Album[]>{
+    return this.http.get<Album[]>(`${environment.baseUrl}album`)
+  }
 }
