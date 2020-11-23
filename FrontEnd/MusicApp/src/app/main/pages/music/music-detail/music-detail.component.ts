c0f89b9d55f7fa19f97e1a6fad1aaebd11897015
samuel14find <bicalho.sam@gmail.com>
2020-11-23 11:59:38 -0300

    Trabalhamos o componente de detalhes da música para trazer a musica da api

--- FrontEnd/MusicApp/src/app/main/pages/music/music-detail/music-detail.component.ts
+++ FrontEnd/MusicApp/src/app/main/pages/music/music-detail/music-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import Album from 'app/model/album';
+import { MusicService } from 'app/services/music.service';
 
 @Component({
   selector: 'app-music-detail',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./music-detail.component.scss']
 })
 export class MusicDetailComponent implements OnInit {
+  albumId: String = undefined;
+  album: Album = undefined;
 
-  constructor() { }
+  // Com a classe ActivateRoute vamos conseguir buscar a informação que necessitamos. Nesse caso o identi
+  //ficador da rota. Nesse caso qual Album queremos exibir.
+  // O router é o responsável por navegar entre os componentes. Se estou querendo voltar uma página 
+  // na verdade estou querendo chamar um componente 
+  constructor(private musicService: MusicService,
+              private activeRoute: ActivatedRoute,
+              private router: Router) 
+              { }
 
   ngOnInit() {
+    this.albumId = this.activeRoute.snapshot.paramMap.get("id");// Esse id é o mesmo que definimos lá no music-routing.module
+    this.musicService.getAlbumDetail(this.albumId).subscribe(data=>{
+      this.album = data;
+    })
+  }
+
+  back(){
+    this.router.navigate(["music"]);
   }
 
 }
