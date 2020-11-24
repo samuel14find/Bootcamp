b4d97086354f3d52a71e19fe7195af23e190bd6f
samuel14find <bicalho.sam@gmail.com>
2020-11-23 22:34:23 -0300

    Adicionamos a propriedade email ao modelo User. Vamos usar ela lá no meu onde mostra o usuário logado

--- FrontEnd/MusicApp/src/app/model/user.ts
+++ FrontEnd/MusicApp/src/app/model/user.ts
@@ -3,6 +3,7 @@ import FavoriteMusic from './favoriteMusic';
 export default class User{
     public id?: string;
     public name?: string;
+    public email?: string;
     public photo?: string;
     public favoritMusics: FavoriteMusic[];
 }
